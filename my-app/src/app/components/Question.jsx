7117c897c30a4c3e6a8abfd2a0f9af8cec168657
Sara Thampi <saraannthampi@gmail.com>
2024-04-30 16:01:18 +0100

    buttons

--- my-app/src/app/components/Question.jsx
+++ my-app/src/app/components/Question.jsx
@@ -1,10 +1,20 @@
 export default function Question({ questiontype, questionData }) {
+    function deleteQ(){
+        console.log("deleteQ");
+    }
+
+    function updateQ(){
+        console.log("updateQ");
+    }
+
     return(
         <div className="p-4 border rounded-md shadow-md">
                         <h3 className="text-xl font-bold mb-2">{questionData['Question']}</h3>
                         <p className="mb-1">Answer A: {questionData['AnswerA']}</p>
                         <p className="mb-1">Answer B: {questionData['AnswerB']}</p>
                         <p className="mb-1">True Answer: {questionData['TrueAnswer']}</p> 
+                        {questiontype === "updatebyID" && <button className="bg-zinc-400 p-3 rounded-lg" onClick={updateQ}>Update</button>}
+                        {questiontype === "deletebyID" && <button className="bg-zinc-400 p-3 rounded-lg" onClick={deleteQ}>Delete</button>}
                     </div>
     )
 }    
