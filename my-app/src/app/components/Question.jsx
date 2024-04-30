e930b48664cb91ed4188becf242951fa59b44f63
Sara Thampi <saraannthampi@gmail.com>
2024-04-30 19:46:23 +0100

    button style

--- my-app/src/app/components/Question.jsx
+++ my-app/src/app/components/Question.jsx
@@ -13,8 +13,8 @@ export default function Question({ questiontype, questionData }) {
                         <p className="mb-1">Answer A: {questionData['AnswerA']}</p>
                         <p className="mb-1">Answer B: {questionData['AnswerB']}</p>
                         <p className="mb-1">True Answer: {questionData['TrueAnswer']}</p> 
-                        {questiontype === "updatebyID" && <button className="bg-zinc-400 p-3 rounded-lg cursor-pointer" onClick={updateQ}>Update</button>}
-                        {questiontype === "deletebyID" && <button className="bg-zinc-400 p-3 rounded-lg cursor-pointer" onClick={()=>deleteQById(questionData["Id"])}>Delete</button>}
+                        {questiontype === "updatebyID" && <button className="bg-zinc-400 text-zinc-50 px-4 py-2 mt-2 rounded-lg border border-zinc-700 hover:bg-zinc-600 transition duration-300 ease-in-out cursor-pointer" onClick={updateQ}>Update</button>}
+                        {questiontype === "deletebyID" && <button className="bg-zinc-400 text-zinc-50 px-4 py-2 mt-2 rounded-lg border border-zinc-700 hover:bg-zinc-600 transition duration-300 ease-in-out cursor-pointer" onClick={()=>deleteQById(questionData["Id"])}>Delete</button>}
                     </div>
     )
 }    
